de8a4f4edfc8c4c1933b0fe78ce331a74e3f0016
msivaprasanth <msivaprasanth5599@gmail.com>
2026-05-28 23:13:46 +0530

    Update seed_data.py

--- backend/seed_data.py
+++ backend/seed_data.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if BASE_DIR not in sys.path:
     sys.path.insert(0, BASE_DIR)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'breathe_esg.settings')
 
 django.setup()
 
